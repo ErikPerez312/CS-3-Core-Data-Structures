810dac1487adfcb1e880ef9d8e08fa6dafaf21ab
ErikPerez312 <theerikperez@gmail.com>
2017-10-30 03:33:31 -0700

    Refactored recursion

--- source/recursion.py
+++ source/recursion.py
@@ -8,21 +8,18 @@ def factorial(n):
         raise ValueError('factorial is undefined for n = {}'.format(n))
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
+
     return factorial_iterative(n)
     # return factorial_recursive(n)
 
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
-    if n == 0 or n == 1:
-        return 1
-    elif n > 1:
-        total = 1
-        for i in range(n):
-            total = total * (n-i)
-            
-        return total
-    pass
+    total = 1
+    for index in range(n):
+        total *= (n - index)
+
+    return total
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
 
